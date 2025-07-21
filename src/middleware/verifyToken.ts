e7d6069f1df0e5e8214a532785b2d559f306c1de
Saltina <saltinaenquiries@gmail.com>
2025-07-21 15:24:14 +0100

    mobile UX debug

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+
+  console.log("Cookies:", req.cookies);
+
   const authHeader = req.headers.authorization;
   const token = authHeader?.split(" ")[1] || req.cookies.access_token;
 
