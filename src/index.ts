63d86ab40a0316f67c6e9bd60634863ec1f480b6
Saltina <saltinaenquiries@gmail.com>
2025-07-21 18:09:58 +0100

    FE url set as cors

--- src/index.ts
+++ src/index.ts
@@ -22,27 +22,25 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// const allowedOrigins = [
-//   "https://stacklog-client.vercel.app"
-// ];
+const allowedOrigins = [
+  "https://stacklog-client.vercel.app",
+];
 
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true,
-// }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // allow cookies to be sent
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-
-app.use(cors({
-  origin: "*", // Allow all origins
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"], // Add more if needed
-}));
 
 
 app.use(cookieParser());
